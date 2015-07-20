6df53a605281c9f4e178784c62fbbe0f8272f7b8
Nick Matantsev <nick.matantsev@gmail.com>
2015-07-20 00:46:27 -0400

    Cleaning up runner CLI

--- run.js
+++ run.js
@@ -1,8 +1,21 @@
 
 var fs = require('fs');
+var path = require('path');
 var express = require('express');
+var minimist = require('minimist');
 
 var RemoteControl = require('./index.js');
+
+var argv = require('minimist')(process.argv.slice(2));
+
+if (argv._.length !== 2) {
+    throw new Error('need two arguments: server module and client module');
+}
+
+var serverModulePath = path.resolve(argv._[0]);
+var clientModulePath = path.resolve(argv._[1]);
+var host = '0.0.0.0';
+var port = argv.port || 3000;
 
 var app = express();
 
@@ -11,8 +24,10 @@ app.get('/', function(request, response) {
     response.end('<html><head><title>RemoteControl Runner</title></head><body><script src="index.js"></script></body></html>');
 });
 
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(port, host);
 
-var rc = new RemoteControl(require('./example/server.js'), server, 'example/index.js');
+var rc = new RemoteControl(require(serverModulePath), server, clientModulePath);
 
 app.get('/index.js', rc.clientMiddleware);
+
+console.info('RemoteControl runner listening on ' + host + ':' + port);
